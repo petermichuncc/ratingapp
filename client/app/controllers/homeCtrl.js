b0bcc787c1540805caf8656fc76ddea41a1c4e6d
petermichuncc <plarrimo@uncc.edu>
2016-04-06 12:10:49 -0400

    Working on the home page.

--- client/app/controllers/homeCtrl.js
+++ client/app/controllers/homeCtrl.js
@@ -1,10 +1,18 @@
 angular
   .module('myApp')
-  .controller('homeCtrl', ['$scope', function($scope) {
+  .controller('homeCtrl', ['$scope','$timeout', function($scope,$timeout) {
     $scope.title = "Home";
 
     $scope.items = ['home','about','contact'];
+    $scope.display = function() {
+  
+        $scope.alertDisplayed = true;
+      $timeout(function() {
+        $scope.alertDisplayed = false;
+      }, 5000)
+    };
     
+    $scope.alertDisplayed = false;
 
   
   }]);
